21138d433a9e1fe7a99d8277799650bc58e1de87
Daniel Hart <me@kingdingdan.com>
2014-12-17 12:13:31 +0000

    Styled login

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include_once('templates/header.php'); ?>
 
 	<div class="content">
-	Hello
+		This is a holding page. This is the least of our worries right now.
 	</div>
 
 
--- templates/header.php
+++ templates/header.php
@@ -14,4 +14,27 @@
 <div class="wrapper">
 	<div class="nav">
 		<div id="logo">LeadTheBoard!</div>
+		<div class="user" id="login" onClick="toggleLogin"> Login/Register </div>
+		<div class="user" id="loggedIn" style="display:none;"> Hello, Name <span class="downArrow">&#9660;</span></div>
+	</div>
+	<div class="loginContainers">
+		<div class="loginForm">
+			<div class="loginDiv">
+			<h3> Login to LeadTheBoard! </h3>
+			<form style="overflow: auto;">
+				<input class="field" type="email" placeholder="Email Address">
+				<input class="field" type="password" placeholder="Password">
+				<div class="formSubmit">
+					<p> Forgot password? </p>
+					<input type="submit" class="submit" value="Login">
+				</div>
+			</form>
+			</div>
+			<div class="loginDiv formBottom">
+				<p>Get a free account</p> <button class="register"> Register </button>
+			</div>
+		</div>
+		<div class="registerForm" style="display:none;">
+
+		</div>
 	</div>
