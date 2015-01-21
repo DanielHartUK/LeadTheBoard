8238023a8dcf98af0345b9ab3c2b536311829f7d
Daniel Hart <me@kingdingdan.com>
2015-01-21 10:20:26 +0000

    Temp fixes

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $servername = "localhost";  // URL of MySQL database, localhost will usually be fine
 $username = "test"; // MySQL username
 $password = "test"; // User password
-$dbname = "testx"; // Database 
+$dbname = "test"; // Database 
 
 
 
--- install.php
+++ install.php
@@ -10,11 +10,11 @@ $DBa = "CREATE TABLE `AchievementProgress` (
   `AchievementID` int(32) unsigned NOT NULL,
   `AchievementProgress` int(3) unsigned NOT NULL,
   `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`UserID`),
   KEY `Achievements` (`AchievementID`),
-  CONSTRAINT `Achievements` FOREIGN KEY (`AchievementID`) REFERENCES `Achievements` (`AchievementID`),
-  CONSTRAINT `Achievement Progress` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`)
-) ENGINE=InnoDB ";
+  KEY `UserID` (`UserID`),
+  CONSTRAINT `Achievement Progress` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Achievements` FOREIGN KEY (`AchievementID`) REFERENCES `Achievements` (`AchievementID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
 
 $DBb = "CREATE TABLE `Achievements` (
   `AchievementID` int(32) unsigned NOT NULL,
@@ -23,49 +23,52 @@ $DBb = "CREATE TABLE `Achievements` (
   `XPValue` int(11) NOT NULL,
   `Icon` text NOT NULL,
   PRIMARY KEY (`AchievementID`)
-) ENGINE=InnoDB ";
+) ENGINE=InnoDB DEFAULT CHARSET=latin1; ";
 
 $DBc = "CREATE TABLE `ClassMembers` (
   `UserID` int(32) unsigned NOT NULL,
   `ClassID` int(32) unsigned DEFAULT NULL,
   PRIMARY KEY (`UserID`),
   KEY `Classes` (`ClassID`),
-  CONSTRAINT `Class Members` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
+  CONSTRAINT `Class Members` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `Classes` FOREIGN KEY (`ClassID`) REFERENCES `Classes` (`ClassID`)
-) ENGINE=InnoDB ";
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
 
 $DBd = "CREATE TABLE `Classes` (
   `ClassID` int(32) unsigned NOT NULL,
-  `Name` text NOT NULL
-) ENGINE=InnoDB ";
+  `Name` text NOT NULL,
+  PRIMARY KEY (`ClassID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
 
 $DBe = "CREATE TABLE `QuestProgress` (
   `UserID` int(32) unsigned NOT NULL,
   `QuestID` int(32) unsigned NOT NULL,
   `QuestProgress` int(3) unsigned NOT NULL,
   `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`UserID`),
   KEY `Quests` (`QuestID`),
-  CONSTRAINT `Quest Progress` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
-  CONSTRAINT `Quests` FOREIGN KEY (`QuestID`) REFERENCES `Quests` (`QuestID`)
- ) ENGINE=InnoDB ";
+  KEY `Quests Progress` (`UserID`),
+  CONSTRAINT `Quests` FOREIGN KEY (`QuestID`) REFERENCES `Quests` (`QuestID`),
+  CONSTRAINT `Quests Progress` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
 
 $DBf = "CREATE TABLE `Quests` (
   `QuestID` int(32) unsigned NOT NULL,
   `Name` text NOT NULL,
   `Description` text NOT NULL,
   `XPValue` int(11) NOT NULL,
-  `Icon` text,
+  `Icon` text NOT NULL,
+  `Expire` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`QuestID`)
-) ENGINE=InnoDB ";
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 $DBg = "CREATE TABLE `UserOptions` (
   `UserID` int(32) unsigned NOT NULL,
-  `Admin` tinyint(1) NOT NULL DEFAULT '0',
   `ColourScheme` tinyint(1) NOT NULL DEFAULT '0',
+  `TimeZone` text NOT NULL,
+  `Clan` text NOT NULL,
   PRIMARY KEY (`UserID`),
-  CONSTRAINT `User Options` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`)
-) ENGINE=InnoDB ";
+  CONSTRAINT `User Options` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 $DBh = "CREATE TABLE `Users` (
   `UserID` int(32) unsigned NOT NULL,
@@ -73,15 +76,22 @@ $DBh = "CREATE TABLE `Users` (
   `Surname` text NOT NULL,
   `Email` text NOT NULL,
   `Avatar` text NOT NULL,
+  `Admin` tinyint(1) DEFAULT '0',
+  `XP` int(255) DEFAULT '0',
+  `Position` int(255) DEFAULT NULL,
+  `Password` text,
+  `Salt` text,
   PRIMARY KEY (`UserID`)
-) ENGINE=InnoDB ";
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ";
 
 $DBi = "CREATE TABLE `XPAwards` (
   `UserID` int(32) unsigned NOT NULL,
   `XP` int(11) NOT NULL,
   `Description` text NOT NULL,
-  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB";
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `XP` (`UserID`),
+  CONSTRAINT `XP` FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
 
 
 mysql_select_db($dbname);
